c7e5c84a826a861b087328dfda6c345ca6121696
Max Lebedev <maxlebedev@live.com>
2015-09-14 11:06:10 -0400

    removed debug print, fixed error print

--- hw1/life.c
+++ hw1/life.c
@@ -6,13 +6,14 @@
 int strtoint(char* str){
 	char* end;
 	int ret = strtol(str, &end, 10);//TODO this error is not being returned
-	if(!end) //conversion error
-		fprintf(stderr, "ERROR\n");
-		//puts("ERROR"); //perror("string could not be converted to int");
+	if(str == end){ //conversion error
+		fprintf(stderr, "input could not be parsed\n");
+		exit(1);
+	}
 	return ret;
 }
 
-//pretty-print the
+//pretty-print the world
 void printArray(int* lifeArray, int row, int col, int currGen){
 	char* outStr = (char *)malloc(sizeof(char)*(row)*(col+1));
 	int index = 0;
@@ -36,12 +37,11 @@ void printRawArray(int* lifeArray, int size){
 }
 
 //create the Life array, read in the init file and bring the appropriate cells to life
-int* createLife(int row, int col, char* filename){ //currently assuming the file is good
+int* createLife(int row, int col, char* filename){ //if the input file assumes the world is bigger than it actually is, bad things happen.
 	int* lifeArray = (int *)calloc(row*col, sizeof(int)); //2d is an illusion.
-	//TODO Read in the file here and turn on the appropriate things
 	FILE * file;
 	file = fopen(filename, "r");
-	if (file  == NULL) {
+	if (file == NULL) {
 		fprintf(stderr, "Can't open input file\n");
 		exit(1);
 	}
@@ -49,8 +49,7 @@ int* createLife(int row, int col, char* filename){ //currently assuming the file
 	int currRow = 1;
 	int currCol = 1;
 	while ((tmp = fgetc(file)) != EOF){
-		if('*' == tmp){
-			//set the thing to live
+		if('*' == tmp){//living
 			lifeArray[(row*currCol)+currRow] = 1;
 			currRow++;
 		}
@@ -64,38 +63,28 @@ int* createLife(int row, int col, char* filename){ //currently assuming the file
 	return lifeArray;
 }
 
+//get the total number of living neighbors for a cell
 int countLivingNeighbors(int* lifeArray, int row, int index){
-	int ret = lifeArray[index-row-1]+ lifeArray[index-row]+ lifeArray[index-row+1]+ lifeArray[index-1]+
+	return lifeArray[index-row-1]+ lifeArray[index-row]+ lifeArray[index-row+1]+ lifeArray[index-1]+
 	lifeArray[index+1]+ lifeArray[index+row-1]+ lifeArray[index+row]+ lifeArray[index+row+1];
-
-	if(index == 26){
-		printf("NW:%d, N:%d, NE:%d\n",lifeArray[index-row-1], lifeArray[index-row], lifeArray[index-row+1]);
-		printf("W:%d,        E:%d\n",lifeArray[index-1],  lifeArray[index+1]);
-		printf("SW:%d, S:%d, SE:%d\n",lifeArray[index+row-1], lifeArray[index+row], lifeArray[index+row+1]);
-	}
-
-
-	return ret;
 }
 
+//update the world gen number of times
 void iterateLife(int* lifeArray, int row, int col, int gen){
 	printArray(lifeArray, row, col, 0);
 	for(int currGen = 1; currGen <= gen; currGen++){
 		int* newLifeArray = (int *)calloc(row*col, sizeof(int)); //2d is an illusion.
-		//TODO change state based on count (create new array)
 		int index = 0;
 		int neigh = 0;
 		for(int i = 1; i < col-1; i++){
 			for(int j = 1; j < row-1; j++){
 				index = (i*row)+j;
 				neigh = countLivingNeighbors(lifeArray, row, index);
-				printf("index %d neigh: %d\n", index, neigh);
 				if(lifeArray[index]){//it is alive
 					if(neigh < 2 || neigh > 3)
 						newLifeArray[index] = 0;
 					else
 						newLifeArray[index] = 1;
-
 				}
 				else if (neigh == 3){
 					newLifeArray[index] = 1;
@@ -115,7 +104,6 @@ int main(int argc,char* argv[]){
 	char* filename = "life";//TODO what if bigger
 	int gen = 10;
 
-	printf("argc: %d\n", argc);
 	if(argc > 1)
 		row = 2 + strtoint(argv[1]);
 	if(argc > 2)
@@ -127,6 +115,5 @@ int main(int argc,char* argv[]){
 
 	int* lifeArray = createLife(row, col, filename);
 	iterateLife(lifeArray, row, col, gen);
-
 	return 0;
 }
