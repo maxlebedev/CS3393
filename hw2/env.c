b396e50326d19636e2cd563595c158d001fc8e81
Max Lebedev <maxlebedev@live.com>
2015-09-24 22:57:14 -0400

    works perfecty on OSX

--- hw2/env.c
+++ hw2/env.c
@@ -1,47 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "env.h"
 
-
-#define MATCH 1
-extern char **environ;
-
-
-//TODO: check all teh functions for errors
-//TODO: Check variable names
-//TODO: Make actual functions
-//TODO: handle all cases
-
 //TODO: Test on a linux machine
-//TODO: if no cmd, print environ
 
-
-//returns 1 if the 'key' of the env strings match
+//returns 1 if the 'key' of the env entrees match
 int keysMatch(char* str1, char* str2){
 	//compare untill a string is over or we reach the '='
 	for(int i = 0; str1[i] != '\0' && str2[i]!= '\0'; i++){
-		if(str1[i] == '='){//the key matches
+		if(str2[i] == '='){//the key matches
 			return MATCH;
 		}
 		if(str1[i] != str2[i]){
 			return !MATCH;
 		}
 	}
-	return !MATCH; //something went wrong
+	return !MATCH;//new env entree did not contain an '='
 }
 
-//copies the non-null elements of source into desination starting at desination[offset]
+//copies the elements of source into desination starting at desination[offset]
 int populateStringArray(int offset, char* source[], char* destination[]){
 	int elemCopied = 0;
-	for(int j = 0;  source[j] != NULL; j++) {//TODO length error checking;
-		int copy = 1;
+	for(int j = 0;  source[j] != NULL; j++) {
+		int copy = TRUE;
 		//we search the destination for a key, if it is not there then we copy
-		for(int i =
-				0; i<offset; i++){
+		for(int i = 0; i<offset; i++){
 			if(keysMatch(destination[i],source[j])){
-				copy = 0;
+				copy = FALSE;
 			}
 		}
 		if(copy){
@@ -58,84 +41,84 @@ void printEnv(){
 	for(int i = 0; environ[i] != NULL; i++){
 		puts(environ[i]);
 	}
+    exit(0);
+}
+
+//check if the -i flag is passed, indicating that a new environ should be made
+int makeNewEnv(char* argv[]){
+    return (!strcmp(argv[1], "-i\0"));
+}
+
+//Return the index where env entrees end and the command to run begins. Also calculate size of new envs
+int findCmdIndex(char* argv[], int argc, int makeNew, int* newEnvSize){
+    char *eqptr;
+    for (int i=1+makeNew; i< argc; i++) {
+        eqptr=strchr(argv[i],'=');
+        if(eqptr !=  NULL){
+            (*newEnvSize)++;
+        }
+        else{
+            return i;
+        }
+    }
+    return argc;
+}
+
+//Ignore the old environment and build a fresh one
+void buildFreshEnviron(char* argv[],int envEnd, char** newEnviron){
+    int j = 0;
+    //argv[0] is env, 1 is -1, we wanna start with 2
+    for(int i = 2; i < envEnd; i++){
+        newEnviron[j] = argv[i];
+        j++;
+    }
+    newEnviron[j] = NULL;
+}
+
+// copy the envs in argv into the newEnviron
+int copyArgvIntoNewEnviton(char* argv[], char** newEnviron, int cmd){
+    int envLen = 0;
+    for(int i = 1; i < cmd; i++){
+        newEnviron[envLen] = argv[i];
+        envLen++;
+    }
+    return envLen;
 }
 
 int main(int argc,char* argv[]){
-	int makeNew = 0;
 	if(argc == 1){
 		printEnv();
-		return 0;
-	}
-	int newEnvSize = 0;
+    }
+    int makeNew = makeNewEnv(argv);
+    int newEnvSize = 0;
 	int cmd = argc;//index of the command being run
-	if(!strcmp(argv[1], "-i\0")) {
-		makeNew = 1;
+
+    cmd = findCmdIndex(argv, argc, makeNew, &newEnvSize);
+
+	char** newEnviron = (char **) malloc(sizeof(char*)*newEnvSize);//this is too large in the case of modified envs
+    if(newEnviron == NULL){
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+
+    //If -i then just build an environ array from argv. If not, copy argv and then add missing keys from old to new environ
+    if(makeNew){
+        buildFreshEnviron(argv, cmd, newEnviron);
 	}
-
-	char *eqptr;
-	//find out where the env entrees end and the command to run begins. Remember the command's index and break out
-	for (int i=1+makeNew; i< argc; i++) {
-		eqptr=strchr(argv[i],'=');
-		if(eqptr !=  NULL){
-			newEnvSize++;
-		}
-		else{
-			cmd=i;
-			break;
-		}
+	else{
+        int envLen = copyArgvIntoNewEnviton(argv, newEnviron, cmd);
+		populateStringArray(envLen, environ, newEnviron);
 	}
-
-
-	//there are 2 options, if -i then just build an env array if not, copy the current env, and modify/add to it
-	char** env;
-	if(makeNew){
-		env = (char **) malloc(sizeof(char*)*newEnvSize);
-		int j = 0;
-		for(int i = 1+makeNew; i < argc; i++){
-			env[j] = argv[i];
-			j++;
-		}
-		env[j] = NULL;
-	}
-	else{//TODO finish this thing. copy argv section into env
-		//lookup new envs in old table to see if it needs to be replaced
-		//int modified = modifyOldEnv(argv, ENV_OFFSET, ENV_OFFSET+newEnvSize);
-		env = (char **) malloc(sizeof(char*)*(newEnvSize));//this is a little too large
-
-		//copy stuff over
-		int j = 0;//TODO fix overflow from nulls?
-		for(int i = 1+makeNew; i < cmd; i++){//TODO doing until cmd is hard if there is no cmd
-			env[j] = argv[i];
-			j++;
-		}
-
-		int elemcopied = populateStringArray(j, environ, env);
-		//TODO if elem copied =0 thats bad, but IDK what to do with that info
-
-	}
-	environ = env;
+	environ = newEnviron;
 
 	if(argc == cmd){
 		printEnv();
-		return 0;
 	}
-
-	char** newArgv;
-	newArgv = (char **) malloc(sizeof(char*)*(argc-cmd));
-	int tmp = 0;
-	for(int i = cmd; i < argc; i++){
-		printf("i = %d\n", i);
-		newArgv[tmp++] = argv[i];
-	}
-	newArgv[tmp] = NULL;
-	puts(*newArgv);
-	execvp(*newArgv, newArgv);//int execvp(const char *file, char *const argv[]);
-
+    //The following requires root privileges to work. This took me far too long to figure out :(
+    execvp(argv[cmd], &argv[cmd]);
 	perror ("The following error occurred");
 	return -1;
 }
-//execvp((argv[cmd]), &argv[cmd]);
-
 
 
 
--- hw2/env.h
+++ hw2/env.h
@@ -1,7 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 
+
+#define MATCH 1
+#define TRUE 1
+#define FALSE 0
+
+extern char **environ;
 
 int keysMatch(char* str1, char* str2);
-int modifyOldEnv(char* envs[],int start,int end);
 int populateStringArray(int offset, char* source[], char* destination[]);
 void printEnv();
+int makeNewEnv(char* argv[]);
+int findCmdIndex(char* argv[], int argc, int makeNew, int* newEnvSize );
+void buildFreshEnviron(char* argv[],int envEnd, char** newEnviron);
+int copyArgvIntoNewEnviton(char* argv[], char** newEnviron, int cmd);
