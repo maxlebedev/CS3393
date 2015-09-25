4a59aaca8394d5e5aab920e545ed97506c7ae72b
Max Lebedev <maxlebedev@live.com>
2015-09-24 21:41:33 -0400

    added env.h

---
+++ hw2/env.h
@@ -0,0 +1,7 @@
+
+
+
+int keysMatch(char* str1, char* str2);
+int modifyOldEnv(char* envs[],int start,int end);
+int populateStringArray(int offset, char* source[], char* destination[]);
+void printEnv();
