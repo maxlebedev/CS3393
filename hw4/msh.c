38cb68e311f12414eee542f6aba5be6dec34ed8b
Max Lebedev <maxlebedev@live.com>
2015-10-07 13:30:17 -0400

    chain2 handles exactly 1 pipe.

--- hw4/msh.c
+++ hw4/msh.c
@@ -15,14 +15,47 @@
 #define MAX_TOK 10
 
 //TODO: make it work with 1 pipe first
-//TODO: we probably don't handle trailing pipes very well
+//TODO: we probably don't handle trailing pipes very well?
 
 static int getLine (char *prmpt, char *buff, size_t size);
 void execute(char* argv[]);
 int shellLoop();
-void parse(char* buff);
-void parse2(char* buff, char * argvv[][MAX_TOK]);
+int parse(char* buff, char * argvv[][MAX_TOK]);
 int countPipes(char* buff);
+int builtin(char* argv[]);
+
+void chain2(char* argvv[][MAX_TOK]){
+	int fd[2];
+	if (pipe(fd) < 0 ){
+		perror("pipe creation failed");
+	}
+	int pid = fork();
+	if(pid == -1){
+		perror("forking  failed");
+	}
+	if(pid == 0){
+		close(1);
+		dup(fd[1]);
+		execvp(argvv[0][0], argvv[0]);//pipe gets closed here
+		perror("exec failed");
+	}
+	else{ //close write end of pipe?
+		wait(NULL);
+		pid = fork();
+		if(pid == 0){
+			close(0);
+			dup(fd[0]);
+			close(fd[1]);//important to close the writing pipe
+			execvp(argvv[1][0], argvv[1]);
+			perror("exec failed");
+		}
+		else{
+			close(fd[1]);
+			close(fd[0]);
+			wait(NULL);
+		}
+	}
+}
 
 int main (int argc, char* argv[]){
 	int done = 0;
@@ -38,11 +71,20 @@ int main (int argc, char* argv[]){
 		int pipes = countPipes(buff);
 		char* argvv[pipes+1][MAX_TOK];
 
-		parse2(buff, argvv);
-		/*execute();
-		 if(argvv != NULL)
-		 	free(argvv);
-			*/
+		int argvCount = parse(buff, argvv);//less of a count, more of a index of last
+		printf("argvCount: %d\n", argvCount);
+		if(argvCount == 1){
+			chain2(argvv);
+		}
+		else{
+			for(int i = 0; i <= argvCount; i++){
+				///printArray(argvv[i]);
+				//check for builtins
+				if(!builtin(argvv[i])){
+					execute(argvv[i]);
+				}
+			}
+		}
 	}
 	return 0;
 }
@@ -77,6 +119,9 @@ int getLine (char *prompt, char *buff, size_t size){
 //spawn a child to run the inputted command with exec
 void execute(char* argv[]){
 	int pid = fork();
+	if(pid == -1){
+		perror("fork failed");
+	}
 	if(pid == 0){
 		execvp(argv[0],argv);
 		perror("exec failed");
@@ -97,30 +142,7 @@ int builtin(char* argv[]){
 	return 0;
 }
 
-//parse the input string into either a builtin (exit) or an argv, to then be passed to execute TODO just parse
-//TODO builtin 
-void parse(char* buff){
-	char* delim = " \t";
-	char* token = strtok(buff, delim);
-	if(token == NULL){
-		perror("strtok failed");
-		exit(1);
-	}
-	int tokCount = 0;
-	char* argv[MAX_TOK];
-	argv[0] = token;
-	//check for builtins
-	
-	//populate the argv array with tokens taken from input
-	while(NULL != token){
-		token = strtok(NULL, delim);
-		argv[++tokCount] = token;
-	}
-	if(!builtin(argv)){
-		execute(argv);
-	}
-}
-
+//TODO delete this and all DB prints
 void printArray(char* argv[]){
 	for(int i = 0; argv[i] != '\0';i++){
 		printf("Array[%d]: %s\n", i, argv[i]);
@@ -128,7 +150,7 @@ void printArray(char* argv[]){
 }
 
 //Parse the input buffer into an argv array. Each entry in the array is a cmd
-void parse2(char* buff, char * argvv[][MAX_TOK]){
+int parse(char* buff, char * argvv[][MAX_TOK]){
 	int argvCount = 0;
 	char* delim = " \t";
 	char* token = strtok(buff, delim);
@@ -140,30 +162,18 @@ void parse2(char* buff, char * argvv[][MAX_TOK]){
 	int tokCount = 1;
 
 	//populate the argv array with tokens taken from input
-	puts("test");
 	while(NULL != token){
 		token = strtok(NULL, delim);
-		puts("test");
-		puts(token);
 		if(token != NULL && !strcmp(token, "|")) {//if we see a pipe, assume a new command and reset arg counter
 			argvv[argvCount][tokCount] = NULL;
 			argvCount++;
 			tokCount = 0;
 		}
 		else{
-			printf("setting [%d][%d] %s\n", argvCount, tokCount, token);
 			argvv[argvCount][tokCount++] = token;
 		}
 	}
-	printf("[%d]\n", argvCount);
-	for(int i = 0; i <= argvCount; i++){
-		printArray(argvv[i]);
-		//check for builtins
-		
-		if(!builtin(argvv[i])){
-			execute(argvv[i]);
-		}
-	}
+	return argvCount;
 }
 int countPipes(char* buff){
 	int pipes = 0;
@@ -174,3 +184,25 @@ int countPipes(char* buff){
 	}
 	return pipes;
 }
+
+
+/*
+//TODO maybe the lowest descendant deals with index = 0
+void daisyChainExecs(char* argvv[][MAX_TOK], int index){
+	if(argvv[index+1] != NULL)//if there is more to be done
+		pipe();
+	int pid = fork();
+	if(pid == -1){
+		//error
+	}
+	if(pid == 0){
+		daisyChainExecs(argvv, index+1){
+			//problem is we want to exec after daisychaining
+			//if they block nicely on iO we can maybe just exec here
+	}
+	else{
+		wait(NULL);
+	}
+
+}
+*/
