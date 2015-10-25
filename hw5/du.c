a1dbd7b0a1a2e1a4d0d99a5a85b27563be4b3423
Max Lebedev <maxlebedev@live.com>
2015-10-24 20:49:46 -0400

    du is now internally consistent in how it handles filesizes

--- hw5/du.c
+++ hw5/du.c
@@ -12,10 +12,15 @@
 //TODO handle symlinks
 //TODO handle hard links
 
+#define DIRSIZE 4
+#define DEF_ARR_LEN 4
+
 int traverseDir(char* dirname, int count);
-int sizeOfDir(char* dir);
+int sizeOfFile(char* dir);
 
 int main (int argc, char* argv[]){
+	int inodeArray[DEF_ARR_LEN] = {0};
+	int inodeSize = 0;
 	char path[PATH_MAX+1];
 	if(!realpath(argv[1], path)){
 		perror("failed to resolve path");
@@ -30,41 +35,43 @@ int traverseDir(char* dirname, int count){
 
 	int catlen = 0; 
 	char *tmpcat = NULL;
-	//tmpcat = (char*) malloc(sizeof(char*)*catlen);
 	long int cumSize = 0;//we might overflow
 	while((direntp = readdir(dirp)) != NULL){
-		if (direntp->d_name[0] != '.') { // By default, skip files starting with '.'
+		if (direntp->d_name[0] != '.') { // Skip files starting with '.'
+			catlen = strlen(dirname) + strlen(direntp->d_name) + 1;//1 extra for '/'
+			tmpcat = (char*) calloc(catlen,  sizeof(char*));
+			strncat(tmpcat, dirname, strlen(dirname));
+			if(tmpcat[strlen(tmpcat)-1] != '/'){//check if end with /, -1 for 0 offset
+				strncat(tmpcat, "/" , 1);
+			}
+			strncat(tmpcat, direntp->d_name, strlen(direntp->d_name));
 			if(direntp->d_type == DT_DIR){ //build the dir string
-				catlen = strlen(dirname) + strlen(direntp->d_name) + 2;//1 extra for '/'m 1 for '\0'
-				tmpcat = (char*) calloc(catlen,  sizeof(char*));
-				strncat(tmpcat, dirname, strlen(dirname));
-				if(tmpcat[strlen(tmpcat)-1] != '/'){//check if end with /, -1 for 0 offset
-					strncat(tmpcat, "/" , 1);
-				}
-				strncat(tmpcat, direntp->d_name, strlen(direntp->d_name));
-
-				//printf("st_blksize: %lld\n",statBuf.st_blocks);
-				//printf("count: %d catlen: %d tmpcat: %s\n", count, catlen, tmpcat);
-				cumSize += traverseDir(tmpcat, count +1);
+				cumSize = traverseDir(tmpcat, count +1);
 				printf("size: %ld : %s\n",cumSize, tmpcat);
-				strncat(tmpcat, "/", 1);//TODO is this needed
+				cumSize = 0;
+				strncat(tmpcat, "/", 1);//TODO is this needed?
 			}
 			else{
-				struct stat statBuf;
-				lstat(direntp->d_name, &statBuf);
-				//printf("%jd/%llu : %s\n", (intmax_t)statBuf.st_size, (long long unsigned int) statBuf.st_blocks, direntp->d_name);//TODO review this cast
-				cumSize += statBuf.st_blocks;
-				//cumSize += statBuf.st_size/statBuf.st_blksize;
+				printf("type: %d\n", direntp->d_type);
+				int add = sizeOfFile(tmpcat);
+				cumSize +=  add;
 			}
 		}
+		else if (direntp->d_name[1] == '\0'){//if we are at .
+			cumSize += DIRSIZE;//TODO check if true on all platforms du seems to do this
+		}
+		
 	}
 	free(tmpcat);
 	closedir(dirp);
-	//return count;
 	return cumSize;
 }
 
 //Takes a directory string an returns its size
-int sizeOfDir(char* dir){
-	return 0;
+int sizeOfFile(char* file){
+	struct stat statBuf;
+	lstat(file, &statBuf);
+	return statBuf.st_blocks;
+//This is not so simple. The linux machine I tested this on had a block size of 4096
+//st_blocks (and /usr/bin/stat) consistent told me files were 2x larger than what du claimed
 }
